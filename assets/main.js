3063d8dc73dc543fb076d0444d866f72eb6c252e
lalitsom <lalitsom27@gmail.com>
2018-12-24 18:18:32 +0530

    first commit

---
+++ assets/main.js
@@ -0,0 +1,9 @@
+function checkMsg() {
+  console.log("checking username and password");
+
+  usr_field = document.getElementById('username_id');
+  pwd_field = document.getElementById('pwd_id');
+
+  
+
+}
